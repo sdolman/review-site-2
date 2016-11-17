7089660e2744924a173e72a8fef389659862ecce
Seth <you@example.com>
2016-11-17 14:19:57 -0500

    iteration 0

---
+++ src/springMvc2/Review.java
@@ -0,0 +1,13 @@
+package springMvc2;
+
+import java.util.Date;
+
+public class Review {
+
+	private long id;
+	private String title;
+	private Date date;
+	private String author;
+	private String content;
+	
+}
